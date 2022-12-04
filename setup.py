48fc1c2acc76a06f40e6ae819a326aa9aacabaa5
aiml5thsem <119796435+aiml5thsem@users.noreply.github.com>
2022-12-04 20:05:06 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,4 @@
 import setuptools
-
-[![forthebadge made-with-python](http://ForTheBadge.com/images/badges/made-with-python.svg)](https://www.python.org/)                 
-[![Python 3.6](https://img.shields.io/badge/python-3.6-blue.svg)](https://www.python.org/downloads/release/python-360/)   
-
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
